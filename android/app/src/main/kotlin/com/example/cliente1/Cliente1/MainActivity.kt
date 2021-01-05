c33d806050de6e1cc3c5ba912c2060676a3807b2
Javier Jonathan Solis Flores <solis.unmsm@gmail.com>
2021-01-04 19:34:54 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/cliente1/Cliente1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cliente1.Cliente1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:Libreria_chat/constants.dart';
+import 'package:Libreria_chat/widget/widget_button_chat.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Cliente 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CHAT_USER_NAME = "Javier Solis";
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(AntDesign.message1),
+              Text(
+                'You have pushed the button this many:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: ButtonChatWidget(
+          urlContactPhoto:
+              "https://crop-circle.imageonline.co/images/picture.png",
+          nameContact: "Raul",
+        ));
+  }
+}
